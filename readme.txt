0d8a5756858b0eb1c3d3a5b0e7d3a491f1b5c488
Doug Bergh <dougbergh@gmail.com>
2015-02-18 21:37:40 -0800

    Removed mongo, fixed readme.txt

--- readme.txt
+++ readme.txt
@@ -1,3 +1,3 @@
-This is a web-app front-end to the SunSpec device probe.
+This is a web-app that imports PVDAQ sites into oSPARC.
 
 It is a MEAN app that runs on port 8082.  Go to <host>:8082 with a browser.
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 // set up ======================================================================
 var express  = require('express');
 var app      = express();					// create our app w/ express
-var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port  	 = process.env.PORT || 8082;			// set the port
-var database = require('./config/database'); 			// load the database config
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var AWS = require('aws-sdk');                               // AWS SDK for access to SES e-mail client
 
 // configuration ===============================================================
-mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public'));	 // set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
